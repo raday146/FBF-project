05f2cf35d9235d755e059a916d70687cd322909e
ShlomoRaday <57037365+raday146@users.noreply.github.com>
2022-02-20 23:25:07 +0200

    old update

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,6 +8,12 @@ import {
   UpdatePassword,
 } from "../controllers/auth.controller";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
+import {
+  CreateProduct,
+  GetProduct,
+  UpdateProduct,
+  DeleteProduct,
+} from "../controllers/product.controller";
 const router = Router();
 
 router.post("/register", Register);
@@ -17,5 +23,9 @@ router.get("/user", AuthenticatedUser);
 router.post("/logout", Logout);
 router.put("/profile", UpdateProfile);
 router.put("/updatepasword", UpdatePassword);
-
+router
+  .post("/create", CreateProduct)
+  .get("product", GetProduct)
+  .put("edit", UpdateProduct)
+  .delete("delete", DeleteProduct);
 export default router;
